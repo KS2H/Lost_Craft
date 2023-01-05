eb949b878ff5465cef73181fdbaaa9f385fa223c
김세환 <115784029+KS2H@users.noreply.github.com>
2023-01-06 00:21:16 +0900

    ✅Update Shark Skill

--- src/main/java/com/sehwen/lostcraft/Skills/SharkSkill.java
+++ src/main/java/com/sehwen/lostcraft/Skills/SharkSkill.java
@@ -17,7 +17,13 @@ public class SharkSkill extends SkillObject implements SkillBase {
 
 	@Override
 	public float getDistance() {
-		return 2;
+		if (tick < 40){
+			return 1;
+		} else if (tick < 50){
+			return 2;
+		} else{
+			return 3;
+		}
 	}
 
 	@Override
@@ -30,35 +36,47 @@ public class SharkSkill extends SkillObject implements SkillBase {
 	@Override
 	public void skillEffect() {
 		tick++;
+		for (int x = -3; x < 4; x++){
+			getLocation().getWorld().spawnParticle(Particle.CRIT_MAGIC, getLocation().getX() + x, getLocation().getY(), getLocation().getZ() + 3, 20, 0.3, 0.3, 0.3, 0.3);
+			getLocation().getWorld().spawnParticle(Particle.CRIT_MAGIC, getLocation().getX() + x, getLocation().getY(), getLocation().getZ() - 3, 20, 0.3, 0.3, 0.3, 0.3);
+		}
+		for (int z = -3; z < 4; z++){
+			getLocation().getWorld().spawnParticle(Particle.CRIT_MAGIC, getLocation().getX() + 3, getLocation().getY(), getLocation().getZ() + z, 20, 0.3, 0.3, 0.3, 0.3);
+			getLocation().getWorld().spawnParticle(Particle.CRIT_MAGIC, getLocation().getX() - 3, getLocation().getY(), getLocation().getZ() + z, 20, 0.3, 0.3, 0.3, 0.3);
+		}
 		if (tick < 20) {
 			teleport(getDirectionVector().multiply(1).add(new Vector(getLocation().getX(), getLocation().getY(), getLocation().getZ())));
 			getLocation().getWorld().spawnParticle(Particle.CRIT_MAGIC, getLocation(), 20, 0.1, 0.1, 0.1, 0.1);
-		} else if (tick == 40) {
-			for (int z = -2; z < 3; z++) {
-				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + 2, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() - 2, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX(), getLocation().getY()+3, getLocation().getZ(), 50, 2, 2, 2, 2);
-			}
-			for (int x = -2; x < 3; x++) {
-				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() + 2), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() - 2), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX(), getLocation().getY()+3, getLocation().getZ(), 50, 2, 2, 2, 2);
+		} else if (tick == 40){
+			for (int x = 0; x < 1; x++) {
+				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ()), EntityType.EVOKER_FANGS);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + x, getLocation().getY()+2, getLocation().getZ(), 50, 1, 1, 1, 1);
 			}
 		} else if (tick == 50) {
 			for (int z = -1; z < 2; z++) {
 				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + 1, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
 				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() - 1, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX(), getLocation().getY()+3, getLocation().getZ(), 50, 2, 2, 2, 2);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + 1, getLocation().getY()+2, getLocation().getZ() + z, 50, 1, 1, 1, 1);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() - 1, getLocation().getY()+2, getLocation().getZ() + z, 50, 1, 1, 1, 1);
 			}
 			for (int x = -1; x < 2; x++) {
 				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() + 1), EntityType.EVOKER_FANGS);
 				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() - 1), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX(), getLocation().getY()+3, getLocation().getZ(), 50, 2, 2, 2, 2);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + 2, getLocation().getY()+2, getLocation().getZ() + x, 50, 1, 1, 1, 1);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() - 2, getLocation().getY()+2, getLocation().getZ() + x, 50, 1, 1, 1, 1);
 			}
-		} else if (tick == 60){
-			for (int x = 0; x < 1; x++) {
-				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ()), EntityType.EVOKER_FANGS);
-				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX(), getLocation().getY()+3, getLocation().getZ(), 50, 2, 2, 2, 2);
+		} else if (tick == 60) {
+			for (int z = -2; z < 3; z++) {
+				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + 2, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
+				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() - 2, getLocation().getY(), getLocation().getZ() + z), EntityType.EVOKER_FANGS);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + 2, getLocation().getY() + 2, getLocation().getZ() + z, 50, 1, 1, 1, 1);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() - 2, getLocation().getY() + 2, getLocation().getZ() + z, 50, 1, 1, 1, 1);
+			}
+			for (int x = -2; x < 3; x++) {
+				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() + 2), EntityType.EVOKER_FANGS);
+				getLocation().getWorld().spawnEntity(new Location(getLocation().getWorld(), getLocation().getX() + x, getLocation().getY(), getLocation().getZ() - 2), EntityType.EVOKER_FANGS);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + x, getLocation().getY() + 2, getLocation().getZ() + 2, 50, 1, 1, 1, 1);
+				getLocation().getWorld().spawnParticle(Particle.FALLING_LAVA, getLocation().getX() + x, getLocation().getY() + 2, getLocation().getZ() - 2, 50, 1, 1, 1, 1);
 			}
 		}
 	}
